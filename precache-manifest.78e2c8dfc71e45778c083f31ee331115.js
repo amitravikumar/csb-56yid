ea76bcdfe30f58996e5a959ff48e762217edcc26
Code Sandbox <hello@codesandbox.io>
2021-05-17 15:33:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.78e2c8dfc71e45778c083f31ee331115.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5810d405a3b7c2d7922",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/css/main.0345e060.chunk.css"
+  },
+  {
+    "revision": "f5810d405a3b7c2d7922",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/js/main.f5810d40.chunk.js"
+  },
+  {
+    "revision": "40ddfdeec3f0a42dddd5",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/css/1.78e2c697.chunk.css"
+  },
+  {
+    "revision": "40ddfdeec3f0a42dddd5",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/js/1.40ddfdee.chunk.js"
+  },
+  {
+    "revision": "dbddee9b8dc63386d0d2",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/js/runtime~main.dbddee9b.js"
+  },
+  {
+    "revision": "dd8b9d284e5e620251336c9158a04db0",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/cover-img.dd8b9d28.jpg"
+  },
+  {
+    "revision": "9a95776761f4e0604c902eb8842e7a50",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/case-1-startup-landing.9a957767.JPG"
+  },
+  {
+    "revision": "265b58c0a33086951ffcafd8596150b0",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/canada.265b58c0.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b3766e9e063afd9dd05e680da17fa066",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/case-2-minimal-ui.b3766e9e.JPG"
+  },
+  {
+    "revision": "28643b234edc4a14db46ddea564e7859",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/case-3-amazon.28643b23.JPG"
+  },
+  {
+    "revision": "7b1ae5404303b9f8245a46f1ee98beb1",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/case-4-friends-portal.7b1ae540.JPG"
+  },
+  {
+    "revision": "717c13a9c0956e93867a273780ee5617",
+    "url": "https://amitravikumar.github.io/csb-56yid/static/media/case-5-event-registration.717c13a9.JPG"
+  },
+  {
+    "revision": "1b17b2ee89a66aa799d7137906566866",
+    "url": "https://amitravikumar.github.io/csb-56yid/index.html"
+  }
+];
